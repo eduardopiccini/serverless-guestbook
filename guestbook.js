18623152aaa8b49b1e28beaf55f33a5f99b1125d
Eduardo Piccini <eduardopicccini@gmail.com>
2022-05-31 17:20:20 -0400

    Cambio url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://1750411d.us-south.apigw.appdomain.cloud/test';
+const apiUrl = 'https://1750411d.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
